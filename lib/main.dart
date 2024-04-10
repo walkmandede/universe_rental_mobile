076b75c54a299ed22fc4a550c3285eb6d4592abe
MayMyat <khinemyat312@gmail.com>
2024-04-10 14:31:23 +0630

    update

--- lib/main.dart
+++ lib/main.dart
@@ -27,69 +27,71 @@ class MyApp extends StatelessWidget {
     SystemChrome.setSystemUIOverlayStyle(
         const SystemUiOverlayStyle(statusBarBrightness: Brightness.light));
     return GetMaterialApp(
-        // key: GlobalKey(),
-        debugShowCheckedModeBanner: false,
-        title: 'App Studio',
-        navigatorObservers: [routeObserver],
-        locale: const Locale('mm', 'MM'),
-        fallbackLocale: const Locale('en', 'US'),
-        theme: ThemeData(
-          primarySwatch: MaterialColor(AppColors.black.value, const {
-            050: Color.fromRGBO(29, 44, 77, .1),
-            100: Color.fromRGBO(29, 44, 77, .2),
-            200: Color.fromRGBO(29, 44, 77, .3),
-            300: Color.fromRGBO(29, 44, 77, .4),
-            400: Color.fromRGBO(29, 44, 77, .5),
-            500: Color.fromRGBO(29, 44, 77, .6),
-            600: Color.fromRGBO(29, 44, 77, .7),
-            700: Color.fromRGBO(29, 44, 77, .8),
-            800: Color.fromRGBO(29, 44, 77, .9),
-            900: Color.fromRGBO(29, 44, 77, 1),
-          }),
-          datePickerTheme: DatePickerTheme.of(context).copyWith(
-              headerForegroundColor: AppColors.white,
-              elevation: 2,
-              shadowColor: AppColors.white.withOpacity(0.4)),
-          // datePickerTheme: DatePickerThemeData(
-          //   shape: RoundedRectangleBorder(
-          //     borderRadius: BorderRadius.circular(15),
-          //   ),
-          //   headerBackgroundColor: AppColors.black,
-          //   backgroundColor: AppColors.white,
-          //   dayStyle: TextStyle(color: AppColors.white),
-          //   weekdayStyle: TextStyle(
-          //     color: AppColors.grey,
-          //     fontWeight: FontWeight.w700
-          //   ),
-          //   yearStyle: TextStyle(
-          //     color: AppColors.white
-          //   ),
-          //   headerHelpStyle: TextStyle(
-          //     color: AppColors.white
-          //   ),
-          //   headerHeadlineStyle: TextStyle(
-          //     color: AppColors.white,
-          //   ),
-          //   rangePickerHeaderHelpStyle: TextStyle(
-          //     color: AppColors.white
-          //   ),
-          //   todayForegroundColor: MaterialStateColor.resolveWith((states) => AppColors.primary),
-          //   yearForegroundColor: MaterialStateColor.resolveWith((states) => AppColors.white),
-          //   dayForegroundColor: MaterialStateColor.resolveWith((states) => AppColors.white),
-          // ),
-          fontFamily: 'Nunito',
-          fontFamilyFallback: const ['Book'],
-          textTheme: TextTheme(
-            bodyMedium: AppConstants.defaultTextStyle,
-            titleMedium: AppConstants.defaultTextStyle,
-            labelLarge: AppConstants.defaultTextStyle,
-            bodyLarge: AppConstants.defaultTextStyle,
-            bodySmall: AppConstants.defaultTextStyle,
-            titleLarge: AppConstants.defaultTextStyle,
-            titleSmall: AppConstants.defaultTextStyle,
-          ),
-          useMaterial3: false,
-        ));
+      // key: GlobalKey(),
+      debugShowCheckedModeBanner: false,
+      title: 'App Studio',
+      navigatorObservers: [routeObserver],
+      locale: const Locale('mm', 'MM'),
+      fallbackLocale: const Locale('en', 'US'),
+      theme: ThemeData(
+        primarySwatch: MaterialColor(AppColors.black.value, const {
+          050: Color.fromRGBO(29, 44, 77, .1),
+          100: Color.fromRGBO(29, 44, 77, .2),
+          200: Color.fromRGBO(29, 44, 77, .3),
+          300: Color.fromRGBO(29, 44, 77, .4),
+          400: Color.fromRGBO(29, 44, 77, .5),
+          500: Color.fromRGBO(29, 44, 77, .6),
+          600: Color.fromRGBO(29, 44, 77, .7),
+          700: Color.fromRGBO(29, 44, 77, .8),
+          800: Color.fromRGBO(29, 44, 77, .9),
+          900: Color.fromRGBO(29, 44, 77, 1),
+        }),
+        datePickerTheme: DatePickerTheme.of(context).copyWith(
+            headerForegroundColor: AppColors.white,
+            elevation: 2,
+            shadowColor: AppColors.white.withOpacity(0.4)),
+        // datePickerTheme: DatePickerThemeData(
+        //   shape: RoundedRectangleBorder(
+        //     borderRadius: BorderRadius.circular(15),
+        //   ),
+        //   headerBackgroundColor: AppColors.black,
+        //   backgroundColor: AppColors.white,
+        //   dayStyle: TextStyle(color: AppColors.white),
+        //   weekdayStyle: TextStyle(
+        //     color: AppColors.grey,
+        //     fontWeight: FontWeight.w700
+        //   ),
+        //   yearStyle: TextStyle(
+        //     color: AppColors.white
+        //   ),
+        //   headerHelpStyle: TextStyle(
+        //     color: AppColors.white
+        //   ),
+        //   headerHeadlineStyle: TextStyle(
+        //     color: AppColors.white,
+        //   ),
+        //   rangePickerHeaderHelpStyle: TextStyle(
+        //     color: AppColors.white
+        //   ),
+        //   todayForegroundColor: MaterialStateColor.resolveWith((states) => AppColors.primary),
+        //   yearForegroundColor: MaterialStateColor.resolveWith((states) => AppColors.white),
+        //   dayForegroundColor: MaterialStateColor.resolveWith((states) => AppColors.white),
+        // ),
+        fontFamily: 'Nunito',
+        fontFamilyFallback: const ['Book'],
+        textTheme: TextTheme(
+          bodyMedium: AppConstants.defaultTextStyle,
+          titleMedium: AppConstants.defaultTextStyle,
+          labelLarge: AppConstants.defaultTextStyle,
+          bodyLarge: AppConstants.defaultTextStyle,
+          bodySmall: AppConstants.defaultTextStyle,
+          titleLarge: AppConstants.defaultTextStyle,
+          titleSmall: AppConstants.defaultTextStyle,
+        ),
+        useMaterial3: false,
+      ),
+      home: homeWidget(),
+    );
     // datePickerTheme: DatePickerThemeData(
     //   shape: RoundedRectangleBorder(
     //     borderRadius: BorderRadius.circular(15),
