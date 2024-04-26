b08d8b27d88af8f99c403ccf72168a0b02606848
MayMyat <khinemyat312@gmail.com>
2024-04-26 10:36:43 +0630

    update

--- lib/modules/home/explore/sub_widgets/listing/w_listing_data_widget.dart
+++ lib/modules/home/explore/sub_widgets/listing/w_listing_data_widget.dart
@@ -15,7 +15,7 @@ import '../../../../../constants/app_functions.dart';
 
 class ListingDataWidget extends StatelessWidget {
   final Size baseSize;
-  const ListingDataWidget({super.key,required this.baseSize});
+  const ListingDataWidget({super.key, required this.baseSize});
 
   @override
   Widget build(BuildContext context) {
@@ -23,20 +23,18 @@ class ListingDataWidget extends StatelessWidget {
     ListingController listingController = Get.find();
     return Padding(
       padding: EdgeInsets.symmetric(
-        horizontal: AppConstants.basePadding,
-        vertical: AppConstants.basePadding
-      ),
+          horizontal: AppConstants.basePadding,
+          vertical: AppConstants.basePadding),
       child: ValueListenableBuilder(
         valueListenable: controller.xUpdatingShownList,
         builder: (context, xUpdatingShownList, child) {
-          if(xUpdatingShownList){
+          if (xUpdatingShownList) {
             return const ListingShimmerWidget();
-          }
-          else{
+          } else {
             return ValueListenableBuilder(
               valueListenable: controller.shownListing,
               builder: (context, shownListing, child) {
-                if(shownListing.isEmpty){
+                if (shownListing.isEmpty) {
                   return Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Center(
@@ -48,12 +46,10 @@ class ListingDataWidget extends StatelessWidget {
                       ),
                     ),
                   );
-                }
-                else{
+                } else {
                   return ListView.builder(
-                    padding: EdgeInsets.only(
-                      bottom: AppConstants.baseNaviBarHeight
-                    ),
+                    padding:
+                        EdgeInsets.only(bottom: AppConstants.baseNaviBarHeight),
                     shrinkWrap: true,
                     physics: const NeverScrollableScrollPhysics(),
                     itemCount: shownListing.length,
@@ -61,71 +57,78 @@ class ListingDataWidget extends StatelessWidget {
                       final each = shownListing[index];
                       var dateString = "-";
                       var nightDataString = "-";
-                      if(each.nightData.isNotEmpty){
-                        dateString = AppFunctions().getDateRangeString(firstDate: each.nightData.first.date, lastDate: each.nightData.last.date);
-                        if(each.nightData.first.nightFees.isNotEmpty){
-                          nightDataString = "${each.nightData.first.nightFees.first}";
+                      if (each.nightData.isNotEmpty) {
+                        dateString = AppFunctions().getDateRangeString(
+                            firstDate: each.nightData.first.date,
+                            lastDate: each.nightData.last.date);
+                        if (each.nightData.first.nightFees.isNotEmpty) {
+                          nightDataString =
+                              "${each.nightData.first.nightFees.first}";
                         }
                       }
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          AspectRatio(
-                            aspectRatio: 1,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(AppConstants.baseBorderRadius),
-                              child: SizedBox.expand(
-                                child: PageView(
-                                  children: [
-                                    ...each.imageList.map((e) {
-                                      return CachedNetworkImage(
-                                        imageUrl: e.getServerPath(),
-                                        fit: BoxFit.cover,
-                                        errorWidget: (context, url, error) {
-                                          return Container(
-                                            width: double.infinity,
-                                            height: double.infinity,
-                                            alignment: Alignment.center,
-                                            decoration: BoxDecoration(
-                                              border: Border.all(),
-                                              borderRadius: BorderRadius.circular(AppConstants.baseBorderRadius)
-                                            ),
-                                            child: const Icon(Icons.image_not_supported_rounded),
+                      return GestureDetector(
+                        onTap: () {
+                          // Get.to(()=>)
+                        },
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            AspectRatio(
+                                aspectRatio: 1,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(
+                                      AppConstants.baseBorderRadius),
+                                  child: SizedBox.expand(
+                                    child: PageView(
+                                      children: [
+                                        ...each.imageList.map((e) {
+                                          return CachedNetworkImage(
+                                            imageUrl: e.getServerPath(),
+                                            fit: BoxFit.cover,
+                                            errorWidget: (context, url, error) {
+                                              return Container(
+                                                width: double.infinity,
+                                                height: double.infinity,
+                                                alignment: Alignment.center,
+                                                decoration: BoxDecoration(
+                                                    border: Border.all(),
+                                                    borderRadius: BorderRadius
+                                                        .circular(AppConstants
+                                                            .baseBorderRadius)),
+                                                child: const Icon(Icons
+                                                    .image_not_supported_rounded),
+                                              );
+                                            },
                                           );
-                                        },
-                                      );
-                                    }).toList()
-                                  ],
-                                ),
-                              ),
-                            )
-                          ),
-                          5.heightBox(),
-                          Text(
-                            each.title,
-                            style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: AppConstants.fontSizeM
+                                        }).toList()
+                                      ],
+                                    ),
+                                  ),
+                                )),
+                            5.heightBox(),
+                            Text(
+                              each.title,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: AppConstants.fontSizeM),
                             ),
-                          ),
-                          Text(
-                            each.subTitle,
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: AppConstants.fontSizeM,
-                                color: AppColors.textGrey
+                            Text(
+                              each.subTitle,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: AppConstants.fontSizeM,
+                                  color: AppColors.textGrey),
                             ),
-                          ),
-                          Text(
-                            dateString,
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: AppConstants.fontSizeM,
-                                color: AppColors.textGrey
+                            Text(
+                              dateString,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: AppConstants.fontSizeM,
+                                  color: AppColors.textGrey),
                             ),
-                          ),
-                          (AppConstants.basePadding).heightBox(),
-                        ],
+                            (AppConstants.basePadding).heightBox(),
+                          ],
+                        ),
                       );
                     },
                   );
