65a3d40dddd70124e912977c6939ee8490e739bd
MayMyat <khinemyat312@gmail.com>
2024-04-26 14:38:08 +0630

    firbase update

--- lib/modules/home/listing_detail/c_listing_detail.dart
+++ lib/modules/home/listing_detail/c_listing_detail.dart
@@ -1,29 +1,45 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:universe_rental/services/network_services/api_end_points.dart';
+import 'package:universe_rental/services/network_services/api_service.dart';
+import 'package:universe_rental/services/overlays_services/dialog/dialog_service.dart';
+import 'package:universe_rental/web_data_entry/all_listings/m_listing_model.dart';
 
-class ListingDetailController extends GetxController{
-
+class ListingDetailController extends GetxController {
   ValueNotifier<int> currentShownPageIndex = ValueNotifier(0);
-
+  ValueNotifier<ListingModel?> listingData = ValueNotifier(null);
   @override
   void onInit() {
     //
     super.onInit();
   }
-  
+
   @override
   void onClose() {
     // TODO: implement onClose
     super.onClose();
   }
-  
-  Future<void> initLoad({required String id,required int shownPageIndex}) async{
+
+  Future<void> initLoad(
+      {required String id, required int shownPageIndex}) async {
     currentShownPageIndex.value = shownPageIndex;
   }
 
-  void onPageChanged(int index){
+  void onPageChanged(int index) {
     currentShownPageIndex.value = index;
   }
-  
+
+  Future<void> getListingData(String id) async {
+    Response? response = await ApiService()
+        .get(endPoint: "${ApiEndPoints.dataEntryListing}/$id");
+
+    final apiResponse = ApiService().validateResponse(response: response);
+
+    if (apiResponse.xSuccess) {
+      listingData.value = ListingModel.fromApi(apiResponse.bodyData['_data']);
+      listingData.notifyListeners();
+    } else {
+      DialogService().showTransactionDialog(text: "Something went wrong");
+    }
+  }
 }
--- lib/modules/home/listing_detail/v_listing_detail_page.dart
+++ lib/modules/home/listing_detail/v_listing_detail_page.dart
@@ -1,7 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 import 'package:get/get.dart';
+import 'package:universe_rental/constants/app_colors.dart';
 import 'package:universe_rental/constants/app_constants.dart';
 import 'package:universe_rental/constants/app_functions.dart';
 import 'package:universe_rental/modules/_common/flutter_super_scaffold.dart';
@@ -14,7 +16,11 @@ class ListingDetailPage extends StatelessWidget {
   final String id;
   final List<String> images;
   final int imageShownIndex;
-  ListingDetailPage({super.key,required this.id,required this.images,required this.imageShownIndex});
+  ListingDetailPage(
+      {super.key,
+      required this.id,
+      required this.images,
+      required this.imageShownIndex});
   final controller = Get.put(ListingDetailController());
 
   @override
@@ -23,9 +29,7 @@ class ListingDetailPage extends StatelessWidget {
     superPrint(imageShownIndex);
     return FlutterSuperScaffold(
       isTopSafe: false,
-      superBarColor: SuperBarColor(
-        topBarColor: Colors.transparent
-      ),
+      superBarColor: SuperBarColor(topBarColor: Colors.transparent),
       onWillPop: () {
         // EachListingWidgetController eachListingWidgetController = Get.find();
         // eachListingWidgetController.movePage(pageIndex: controller.currentShownPageIndex.value, id: id);
@@ -35,36 +39,130 @@ class ListingDetailPage extends StatelessWidget {
           SingleChildScrollView(
             child: Column(
               children: [
-                AspectRatio(
-                  aspectRatio: 1,
-                  child: Hero(
-                    tag: "imageList$id",
-                    child: PageView(
-                      controller: PageController(initialPage: imageShownIndex),
-                      onPageChanged: (value) {
-
-                      },
-                      children: [
-                        ...images.map((e) {
-                          return CachedNetworkImage(
-                            imageUrl: e.getServerPath(),
-                            fit: BoxFit.cover,
-                            errorWidget: (context, url, error) {
-                              return Container(
-                                width: double.infinity,
-                                height: double.infinity,
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                    border: Border.all(),
-                                    borderRadius: BorderRadius.circular(AppConstants.baseBorderRadius)
-                                ),
-                                child: const Icon(Icons.image_not_supported_rounded),
-                              );
-                            },
-                          );
-                        }).toList()
-                      ],
-                    ),
+                //   AspectRatio(
+                //     aspectRatio: 1,
+                //     child: Hero(
+                //       tag: "imageList$id",
+                //       child: PageView(
+                //         controller: PageController(initialPage: imageShownIndex),
+                //         onPageChanged: (value) {
+                //         },
+                //         children: [
+                //           ...images.map((e) {
+                //             return CachedNetworkImage(
+                //               imageUrl: e.getServerPath(),
+                //               fit: BoxFit.cover,
+                //               errorWidget: (context, url, error) {
+                //                 return Container(
+                //                   width: double.infinity,
+                //                   height: double.infinity,
+                //                   alignment: Alignment.center,
+                //                   decoration: BoxDecoration(
+                //                       border: Border.all(),
+                //                       borderRadius: BorderRadius.circular(AppConstants.baseBorderRadius)
+                //                   ),
+                //                   child: const Icon(Icons.image_not_supported_rounded),
+                //                 );
+                //               },
+                //             );
+                //           }).toList()
+                //         ],
+                //       ),
+                //     ),
+                //   )
+                Container(
+                  color: Colors.red,
+                  width: double.infinity,
+                  height: 300,
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 22, vertical: 18),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'HER HUAHIN POOL VILLA'.toUpperCase(),
+                        style: const TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.w700),
+                      ),
+                      (Get.height * 0.02).heightBox(),
+                      const Text(
+                        'Entire villa in Tambon Hua Hin, Thailand',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w600),
+                      ),
+                      (Get.height * 0.01).heightBox(),
+                      Row(
+                        children: List.generate(
+                            4,
+                            (index) => Row(
+                                  children: [
+                                    if (index != 0)
+                                      (Get.width * 0.02).widthBox(),
+                                    const Text('3 beds'),
+                                    if (index != 3)
+                                      (Get.width * 0.02).widthBox(),
+                                    if (index != 3) bubble(),
+                                  ],
+                                )).toList(),
+                      ),
+                      (Get.height * 0.01).heightBox(),
+                      Row(
+                        children: [
+                          const Icon(TablerIcons.star_filled),
+                          4.widthBox(),
+                          const Text(
+                            "4.3",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w500),
+                          ),
+                          8.widthBox(),
+                          bubble(),
+                          8.widthBox(),
+                          const Text(
+                            "15 reviews",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                              decoration: TextDecoration.underline,
+                            ),
+                          )
+                        ],
+                      ),
+                      (Get.height * 0.02).heightBox(),
+                      divider(),
+                      Card(
+                        margin: const EdgeInsets.symmetric(vertical: 16),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 14, vertical: 16),
+                          child: Row(
+                            children: [
+                              const CircleAvatar(
+                                  backgroundImage: AssetImage(
+                                      'assets/images/fakeprofile.png')),
+                              10.widthBox(),
+                              const Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Hosted by Tanakan",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 18),
+                                  ),
+                                  Text(
+                                    "Normalhost . 2 years hosting ",
+                                    style: TextStyle(fontSize: 16),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 )
               ],
@@ -73,17 +171,29 @@ class ListingDetailPage extends StatelessWidget {
           Align(
             alignment: Alignment.topCenter,
             child: Container(
-              padding: EdgeInsets.only(
-                top: Get.mediaQuery.padding.top
-              ),
-              decoration: BoxDecoration(
-                color: Colors.transparent
-              ),
+              padding: EdgeInsets.only(top: Get.mediaQuery.padding.top),
+              decoration: const BoxDecoration(color: Colors.transparent),
               child: const ListingDetailAppBar(),
             ),
           )
         ],
       ),
+    );
+  }
+
+  Widget divider() {
+    return Divider(
+      thickness: 1,
+      color: Colors.grey,
+      height: Get.height * 0.02,
+    );
+  }
+
+  Widget bubble() {
+    return Container(
+      width: Get.width * 0.02,
+      height: Get.height * 0.02,
+      decoration: BoxDecoration(shape: BoxShape.circle, color: AppColors.black),
     );
   }
 }
