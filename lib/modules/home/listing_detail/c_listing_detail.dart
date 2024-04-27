0aaf459a038e29a85b9323c7ca9af8528e5c3acc
MayMyat <khinemyat312@gmail.com>
2024-04-27 11:47:34 +0630

    bind half listing

--- lib/modules/home/listing_detail/c_listing_detail.dart
+++ lib/modules/home/listing_detail/c_listing_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:universe_rental/modules/_common/models/m_listing_detail.dart';
 import 'package:universe_rental/services/network_services/api_end_points.dart';
 import 'package:universe_rental/services/network_services/api_service.dart';
 import 'package:universe_rental/services/overlays_services/dialog/dialog_service.dart';
@@ -7,7 +8,7 @@ import 'package:universe_rental/web_data_entry/all_listings/m_listing_model.dart
 
 class ListingDetailController extends GetxController {
   ValueNotifier<int> currentShownPageIndex = ValueNotifier(0);
-  ValueNotifier<ListingModel?> listingData = ValueNotifier(null);
+  ValueNotifier<ListingDetail?> listingData = ValueNotifier(null);
   @override
   void onInit() {
     //
@@ -23,6 +24,7 @@ class ListingDetailController extends GetxController {
   Future<void> initLoad(
       {required String id, required int shownPageIndex}) async {
     currentShownPageIndex.value = shownPageIndex;
+    getListingData(id);
   }
 
   void onPageChanged(int index) {
@@ -36,7 +38,8 @@ class ListingDetailController extends GetxController {
     final apiResponse = ApiService().validateResponse(response: response);
 
     if (apiResponse.xSuccess) {
-      listingData.value = ListingModel.fromApi(apiResponse.bodyData['_data']);
+      listingData.value =
+          ListingDetail.fromDetail(data: apiResponse.bodyData['_data']);
       listingData.notifyListeners();
     } else {
       DialogService().showTransactionDialog(text: "Something went wrong");
--- lib/modules/home/listing_detail/v_listing_detail_page.dart
+++ lib/modules/home/listing_detail/v_listing_detail_page.dart
@@ -1,14 +1,18 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 import 'package:get/get.dart';
 import 'package:universe_rental/constants/app_colors.dart';
 import 'package:universe_rental/constants/app_constants.dart';
 import 'package:universe_rental/modules/_common/flutter_super_scaffold.dart';
+import 'package:universe_rental/modules/_common/models/m_listing_detail.dart';
 import 'package:universe_rental/modules/home/listing_detail/c_listing_detail.dart';
 import 'package:universe_rental/modules/home/listing_detail/w_listing_detail_app_bar.dart';
 import 'package:universe_rental/modules/my_calendar/_my_calendar_test_page.dart';
 import 'package:universe_rental/services/others/extensions.dart';
+import 'package:universe_rental/web_data_entry/listing_attribute/m_listing_attribute.dart';
 
 class ListingDetailPage extends StatelessWidget {
   final String id;
@@ -74,25 +78,29 @@ class ListingDetailPage extends StatelessWidget {
                 // ),
                 ValueListenableBuilder(
                     valueListenable: controller.listingData,
-                    builder: (context, value, child) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 22, vertical: 18),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            listingData(),
-                            (Get.height * 0.02).heightBox(),
-                            divider(),
-                            hostCard(),
-                            aboutPlace(),
-                            placesImages(),
-                            offerList(),
-                            location(),
-                            booking(),
-                          ],
-                        ),
-                      );
+                    builder: (context, _listing, child) {
+                      if (_listing != null) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 22, vertical: 18),
+                          child: Column(
+                            // crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              listingData(_listing),
+                              (Get.height * 0.02).heightBox(),
+                              divider(),
+                              hostCard(_listing),
+                              // aboutPlace(_listing),
+                              AboutPlaceWidget(listing: _listing),
+                              placesImages(),
+                              offerList(),
+                              location(),
+                              booking(),
+                            ],
+                          ),
+                        );
+                      }
+                      return const SizedBox();
                     })
               ],
             ),
@@ -110,36 +118,60 @@ class ListingDetailPage extends StatelessWidget {
     );
   }
 
-  Widget listingData() {
+  Widget listingData(ListingDetail listing) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          'HER HUAHIN POOL VILLA'.toUpperCase(),
+          listing.title.toUpperCase(),
           style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
         ),
         (Get.height * 0.02).heightBox(),
-        const Text(
-          'Entire villa in Tambon Hua Hin, Thailand',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+        Text(
+          listing.subTitle,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
         ),
         (Get.height * 0.01).heightBox(),
-        Row(
-          children: List.generate(
-              4,
-              (index) => Row(
-                    children: [
-                      if (index != 0) (Get.width * 0.02).widthBox(),
-                      const Text('3 beds'),
-                      if (index != 3) (Get.width * 0.02).widthBox(),
-                      if (index != 3) bubble(),
-                    ],
-                  )).toList(),
+        Wrap(
+          runSpacing: 10,
+          children: [
+            ...listing.listingOnAttributes.map((e) {
+              ListingOnAttribute _attribute = e;
+              int index = listing.listingOnAttributes.indexOf(e);
+              return Row(
+                children: [
+                  if (index != 0) (Get.width * 0.02).widthBox(),
+                  Text(
+                      '${_attribute.quantity} ${_attribute.listingAttribute.name}'),
+                  if (index != 3) (Get.width * 0.02).widthBox(),
+                  if (index != 3) bubble(),
+                ],
+              );
+            })
+          ],
         ),
+        // Wrap(
+        //   direction: Axis.horizontal,
+        //   children: List.generate(listing.listingOnAttributes.length, (index) {
+        //     ListingOnAttribute _attribute = listing.listingOnAttributes[index];
+        //     return Row(
+        //       children: [
+        //         if (index != 0) (Get.width * 0.02).widthBox(),
+        //         Text(
+        //             '${_attribute.quantity} ${_attribute.listingAttribute.name}'),
+        //         if (index != 3) (Get.width * 0.02).widthBox(),
+        //         if (index != 3) bubble(),
+        //       ],
+        //     );
+        //   }).toList(),
+        // ),
         (Get.height * 0.01).heightBox(),
         Row(
           children: [
-            const Icon(TablerIcons.star_filled),
+            const Icon(
+              TablerIcons.star_filled,
+              size: 18,
+            ),
             4.widthBox(),
             const Text(
               "4.3",
@@ -162,11 +194,20 @@ class ListingDetailPage extends StatelessWidget {
     );
   }
 
-  Widget hostCard() {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 16),
+  Widget hostCard(ListingDetail listing) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 20),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(
+                spreadRadius: 4,
+                blurRadius: 10,
+                color: AppColors.grey.withOpacity(0.3))
+          ]),
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 20),
         child: Row(
           children: [
             CircleAvatar(
@@ -174,14 +215,15 @@ class ListingDetailPage extends StatelessWidget {
                 backgroundImage:
                     const AssetImage('assets/images/fakeprofile.png')),
             10.widthBox(),
-            const Column(
+            Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  "Hosted by Tanakan",
-                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+                  "Hosted by ${listing.hostName}",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w500, fontSize: 18),
                 ),
-                Text(
+                const Text(
                   "Normalhost . 2 years hosting ",
                   style: TextStyle(fontSize: 16),
                 )
@@ -193,24 +235,28 @@ class ListingDetailPage extends StatelessWidget {
     );
   }
 
-  Widget aboutPlace() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          'About this space',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-        ),
-        10.heightBox(),
-        const Text(
-          "About this space Location is Huahin soi 88. Hua Hin vacation home style minimalist. private Suitable for guests who are couples up to 4 couples and can also sleep up to 8-12 persons (Maximum 12 persons)",
-          style: TextStyle(fontSize: 16),
-        ),
-        10.heightBox(),
-        topBoxShadow(),
-      ],
-    );
-  }
+  // Widget aboutPlace(ListingDetail listing) {
+
+  //   return Column(
+  //     crossAxisAlignment: CrossAxisAlignment.start,
+  //     children: [
+  //       const Text(
+  //         'About this space',
+  //         style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+  //       ),
+  //       10.heightBox(),
+  //       SizedBox(
+  //         height: Get.height * 0.1,
+  //         child: Text(
+  //           listing.description,
+  //           style: const TextStyle(fontSize: 16),
+  //         ),
+  //       ),
+  //       10.heightBox(),
+  //       topBoxShadow(),
+  //     ],
+  //   );
+  // }
 
   Widget booking() {
     return Column(
@@ -312,58 +358,103 @@ class ListingDetailPage extends StatelessWidget {
           const Text(
               "Nearby Place \n -place near shwedagon \n -near xsphere n -near sule pagoda"),
           (Get.height * 0.01).heightBox(),
-          topBoxShadow()
+          // topBoxShadow()
         ],
       ),
-    );
-  }
-
-  Widget divider() {
-    return Divider(
-      thickness: 1,
-      color: Colors.grey,
-      height: Get.height * 0.02,
     );
   }
 
   Widget bubble() {
     return Container(
-      width: Get.width * 0.02,
-      height: Get.height * 0.02,
+      width: Get.width * 0.01,
+      height: Get.height * 0.01,
       decoration: BoxDecoration(shape: BoxShape.circle, color: AppColors.black),
     );
   }
+}
 
-  Widget topBoxShadow() {
-    return Container(
-      decoration: BoxDecoration(boxShadow: [
-        BoxShadow(
-            offset: Offset(0, -Get.height * 0.05),
-            blurRadius: 4,
-            spreadRadius: 3,
-            color: AppColors.white.withOpacity(0.6))
-      ]),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          divider(),
-          Container(
-            height: Get.height * 0.05,
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                      offset: const Offset(0, 10),
-                      blurRadius: 4,
-                      spreadRadius: 2,
-                      color: AppColors.grey.withOpacity(0.1))
-                ],
-                borderRadius: const BorderRadius.all(Radius.circular(20))),
-            child: const Text("See More"),
+Widget divider() {
+  return Divider(
+    thickness: 1,
+    color: Colors.grey.withOpacity(0.4),
+    height: Get.height * 0.02,
+  );
+}
+
+Widget topBoxShadow(bool xSeeMoreClick, void Function()? onTap) {
+  return Container(
+    decoration: BoxDecoration(boxShadow: [
+      BoxShadow(
+          offset: Offset(0, -Get.height * 0.05),
+          blurRadius: 4,
+          spreadRadius: 3,
+          color: AppColors.white.withOpacity(0.6))
+    ]),
+    child: xSeeMoreClick
+        ? divider()
+        : Stack(
+            alignment: Alignment.center,
+            children: [
+              divider(),
+              GestureDetector(
+                onTap: onTap,
+                child: Container(
+                  height: Get.height * 0.05,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            offset: const Offset(0, 10),
+                            blurRadius: 4,
+                            spreadRadius: 2,
+                            color: AppColors.grey.withOpacity(0.1))
+                      ],
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(20))),
+                  child: const Text("See More"),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+  );
+}
+
+class AboutPlaceWidget extends StatefulWidget {
+  final ListingDetail listing;
+  const AboutPlaceWidget({super.key, required this.listing});
+
+  @override
+  State<AboutPlaceWidget> createState() => _AboutPlaceWidgetState();
+}
+
+class _AboutPlaceWidgetState extends State<AboutPlaceWidget> {
+  bool _xClickSeeMore = false;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'About this space',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+        ),
+        10.heightBox(),
+        SizedBox(
+          height: Get.height * 0.1,
+          child: Text(
+            widget.listing.description,
+            style: const TextStyle(fontSize: 16),
+          ),
+        ),
+        10.heightBox(),
+        topBoxShadow(_xClickSeeMore, () {
+          setState(() {
+            _xClickSeeMore != _xClickSeeMore;
+          });
+        }),
+      ],
     );
   }
 }
