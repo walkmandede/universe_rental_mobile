7b102f265edcf30c0db3293a2c0f2e7bc98b87d6
MayMyat <khinemyat312@gmail.com>
2024-05-08 09:28:42 +0630

    setting pageupdate

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ void main() async {
   } catch (e) {
     null;
   }
+
   runApp(const MyApp());
   // runApp(const RestartAppWidget(child: MyApp()));
 }
@@ -28,6 +29,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // fucn([3, 4, 5, 6, 9]);
+    // toArray('3_6,9_12,24');
     SystemChrome.setSystemUIOverlayStyle(
         const SystemUiOverlayStyle(statusBarBrightness: Brightness.light));
     return GetMaterialApp(
@@ -100,6 +103,49 @@ class MyApp extends StatelessWidget {
         home: homeWidget()
         // home: StatusBarTestingPage1(),
         );
+  }
+
+  List toArray(str) {
+    List _list = [];
+    Set _result = {};
+    _list = str.toString().split("");
+    for (int i = 0; i < _list.length; i++) {
+      print(i);
+      if (_list[i] == "_") {
+        int _num = int.parse(_list[i - 1]);
+        do {
+          _result.add(_num);
+          _num++;
+        } while (_num != int.parse(_list[i + 1]));
+        _result.add(int.parse(_list[i + 1]));
+      } else if (_list[i] == ",") {
+        continue;
+      } else if (i == _list.length - 1 && _list[i - 1] == ",") {
+        _result.add(int.parse(_list[i]));
+      }
+    }
+    print(_result);
+    return [];
+  }
+
+  String fucn(List<int> i) {
+    //[3,4,5,6,9]
+    List<int> arr = [3, 4, 5, 6, 9];
+
+    String _result = arr.first.toString();
+    for (int i = 0; i < arr.length; i++) {
+      if (i < arr.length - 1 && arr[i] + 1 == arr[i + 1]) {
+        print("object");
+      } else {
+        if (arr[i] - 1 == arr[i - 1]) {
+          _result = _result + "_${arr[i]},";
+        } else if (_result.endsWith(",")) {
+          _result = _result + (arr[i]).toString();
+        }
+      }
+    }
+
+    return _result;
   }
 
   Widget homeWidget() {
